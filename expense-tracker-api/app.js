8e7a0fbc1bbcc7ee9ed3d551defdc35f7318b019
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-01-20 22:39:58 +0100

    dependencies added.

--- expense-tracker-api/app.js
+++ expense-tracker-api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+require('dotenv/config')
 const app = express();
-app.get("/",(req,res)=>{
+
+app.get("/", (req, res) => {
     res.send("Hello")
 })
+
 
 app.listen(8000);
